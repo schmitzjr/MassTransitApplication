b3ca33296cd00c4f5c3b9e447a915c5af811d0d2
schmitzjr <schmitzjr@gmail.com>
2022-05-08 21:22:23 -0300

    first commit

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,14 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
 using mass_transit.Mappings;
 using MassTransitApplication.Consumers;
+using MassTransitApplication.Events;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace MassTransitApplication
 {
@@ -30,9 +38,16 @@ namespace MassTransitApplication
             });
             services.AddSingleton(config.CreateMapper());
             #endregion
-            #region MassTransitProducer
+
+            #region MassTransit
             services.AddMassTransit(trans =>
             {
+                trans.AddDelayedMessageScheduler();
+                trans.AddConsumer<CustomerCreatedConsumer>();
+                trans.AddConsumer<CustomerMailListConsumer>();
+                trans.AddRequestClient<ICustomerCreatedEvent>();
+                trans.SetKebabCaseEndpointNameFormatter();
+
                 trans.UsingRabbitMq((ctx, con) =>
                 {
                     con.Host(Configuration.GetConnectionString("AMQPUrl"), cred => 
@@ -40,19 +55,20 @@ namespace MassTransitApplication
                         cred.Username(Configuration.GetConnectionString("AMQPUser"));
                         cred.Password(Configuration.GetConnectionString("AMQPPassword"));
                     });
+
+                    con.UseDelayedMessageScheduler();
+
+                    var options = new ServiceInstanceOptions()
+                        .SetEndpointNameFormatter(ctx.GetService<IEndpointNameFormatter>() ?? KebabCaseEndpointNameFormatter.Instance);
+                    
+                    con.ServiceInstance(options, instance =>
+                    {
+                        instance.ConfigureEndpoints(ctx);
+                    });    
                 });
             });
             #endregion
-            #region MassTransitConsumers
-            var busControl = Bus.Factory.CreateUsingRabbitMq(cfg => 
-            {
-                cfg.ReceiveEndpoint("greet-new-customers", e => 
-                {
-                    e.Consumer<CustomerCreatedConsumer>();
-                });
-            });
-            busControl.StartAsync();
-            #endregion
+            
 
             services.AddScoped<ICreateCustomerService, CreateCustomerService>();
 
@@ -68,11 +84,10 @@ namespace MassTransitApplication
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MassTransitApplication v1"));
             }
 
-            //app.UseHttpsRedirection();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MassTransitApplication v1"));
 
             app.UseRouting();
 
@@ -80,8 +95,29 @@ namespace MassTransitApplication
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapHealthChecks("/health/ready", new HealthCheckOptions
+                {
+                    Predicate = check => check.Tags.Contains("ready"),
+                    ResponseWriter = HealthCheckResponseWriter
+                });
+
+                endpoints.MapHealthChecks("/health/live", new HealthCheckOptions {ResponseWriter = HealthCheckResponseWriter});
                 endpoints.MapControllers();
             });
+        }
+
+        static Task HealthCheckResponseWriter(HttpContext context, HealthReport result)
+        {
+            var json = new JObject(
+                new JProperty("status", result.Status.ToString()),
+                new JProperty("results", new JObject(result.Entries.Select(entry => new JProperty(entry.Key, new JObject(
+                    new JProperty("status", entry.Value.Status.ToString()),
+                    new JProperty("description", entry.Value.Description),
+                    new JProperty("data", JObject.FromObject(entry.Value.Data))))))));
+
+            context.Response.ContentType = "application/json";
+
+            return context.Response.WriteAsync(json.ToString(Formatting.Indented));
         }
     }
 }
